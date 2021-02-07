0c68309bf49e299e1bb35aa3bf270213183987ae
zhouwenfang <clark_zhou@sinobasedm.com>
2021-02-07 14:00:21 +0800

    init project

---
+++ security-base/src/main/java/com/security/base/SecurityBaseApplication.java
@@ -0,0 +1,13 @@
+package com.security.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBaseApplication.class, args);
+    }
+
+}
